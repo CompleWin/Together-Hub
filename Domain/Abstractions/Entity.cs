cd82fe6af45e8b619f371c6660efeef6ccaaebaa
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-12 11:29:04 +0300

    Add IEntity, Entity<T>; Location, TopicId; DomainException

---
+++ Domain/Abstractions/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Abstractions;
+
+public class Entity<T> : IEntity<T>
+{
+    public required T Id { get; set; }
+}
---
+++ Domain/Abstractions/IEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Abstractions;
+
+public interface IEntity<T> : IEntity
+{
+    T Id { get; set; }
+}
+
+public interface IEntity
+{
+    
+}
