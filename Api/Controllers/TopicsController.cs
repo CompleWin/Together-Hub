893ef1cd125963cd82b3933302eb52a6c45ec171
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-12 10:31:55 +0300

    initial commit

---
+++ Api/Controllers/TopicsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TopicsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Hello()
+    {
+        return await Task.FromResult(Ok(new {text = "Hello World"}));
+    }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
+
