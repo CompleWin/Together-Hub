b89d72ee507aa102802d912e1ab5fd75dd278140
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-14 12:33:34 +0300

    Add delete properties to entity

--- Domain/Abstractions/Entity.cs
+++ Domain/Abstractions/Entity.cs
@@ -1,6 +1,8 @@
 ﻿namespace Domain.Abstractions;
 
-public class Entity<T> : IEntity<T>
+public abstract class Entity<T> : IEntity<T>
 {
     public required T Id { get; set; }
+    public bool IsDeleted { get; set; } = default!;
+    public DateTimeOffset? DeletedAt { get; set; } = default!;
 }
