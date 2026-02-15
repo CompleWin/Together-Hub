44b33585e13dbd0c7f340e24b0d53471cdcb34b9
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-15 13:40:16 +0300

    Add AutoMapping CreateTopicDto

--- Application/Topics/Commands/CreateTopic/CreateTopicHandler.cs
+++ Application/Topics/Commands/CreateTopic/CreateTopicHandler.cs
@@ -1,28 +1,15 @@
 ﻿namespace Application.Topics.Commands.CreateTopic;
 
-public class CreateTopicHandler(IApplicationDbContext dbContext) :
+public class CreateTopicHandler(IApplicationDbContext dbContext, IMapper mapper) :
     ICommandHandler<CreateTopicCommand, CreateTopicResult>
 {
     public async Task<CreateTopicResult> Handle(CreateTopicCommand request, CancellationToken ct)
     {
-        Topic newTopic = CreateTopic(request.RequestDto);
+        var newTopic = mapper.Map<Topic>(request.RequestDto);
         
         await dbContext.Topics.AddAsync(newTopic, ct);
         await dbContext.SaveChangesAsync(ct);
         return new CreateTopicResult(newTopic.ToTopicResponseDto());
         
-    }
-
-    private Topic CreateTopic(CreateTopicRequestDto dto)
-    {
-        Topic newTopic = Topic.Create(
-            TopicId.Of(Guid.NewGuid()),
-            dto.Title,
-            dto.EventStart,
-            dto.Summary,
-            dto.TopicType,
-            Location.Of(dto.Location.Street, dto.Location.City)
-            );
-        return newTopic;
     }
 }
