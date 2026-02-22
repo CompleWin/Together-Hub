1496cf6cb9acc7cb3ab7cb0558840532250b6f13
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-22 10:06:03 +0300

    Add Comment entity and value object CommentId

---
+++ Domain/ValueObjects/CommentId.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.ValueObjects;
+
+public record CommentId
+{
+    public Guid Value { get; set; }
+
+    private CommentId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static CommentId Of(Guid value)
+    {
+        if (value == Guid.Empty)
+        {
+            throw new ArgumentException("Id must not be empty.");
+        }
+
+        return new CommentId(value);
+    }
+    
+}
