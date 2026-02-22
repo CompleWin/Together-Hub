27e8dd8d84104dc9de7df6af20578a70e023aa94
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-22 10:11:04 +0300

    Add CommentId configuration; Add Comments to DbContext and to IApplicationDbContext

--- Application/Data/DataBaseContext/IApplicationDbContext.cs
+++ Application/Data/DataBaseContext/IApplicationDbContext.cs
@@ -5,5 +5,6 @@ public interface IApplicationDbContext
     DbSet<Topic> Topics { get; }
     DbSet<Relationship> Relationships { get; }
     DbSet<CustomIdentityUser> Users { get; }
+    DbSet<Comment> Comments { get; }
     Task<int> SaveChangesAsync(CancellationToken ct);
 }
