f4863048d4ebf4f829463e29d7eb529a515ccfa5
Nikolai Ivakhnenko <complewin@yandex.ru>
2026-02-23 19:40:44 +0300

    global using

--- Api/GlobalUsing.cs
+++ Api/GlobalUsing.cs
@@ -10,7 +10,5 @@ global using Application.Topics.Commands.DeleteTopic;
 global using Application.Topics.Commands.UpdateTopic;
 global using Api.Exception.Handler;
 global using Application.Mapping;
-global using Domain.Security;
 global using Microsoft.AspNetCore.Authorization;
 global using System.Text;
-global using Infrastructure.Data.DataBaseContext;
